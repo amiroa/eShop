f080c0f210f6947e8b2e949cc54e0ee8379bd07d
Amir Rahighi <amirmbox@gmail.com>
2025-07-09 20:52:26 +1000

    Get Product By Id and Category.

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Catalog.API.Exceptions;
+
+[Serializable]
+internal class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException(): base("Product not found!")
+    {
+
+    }
+    public ProductNotFoundException(string message) : base(message)
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductByIdQuery(Guid Id): IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+
+internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+        if (product is null)
+        {
+            logger.LogWarning("Product with Id {Id} not found", query.Id);
+            throw new ProductNotFoundException($"Product with Id {query.Id} not found in the catalog.");
+        } else
+        {
+            return new GetProductByIdResult(product);
+        }
+    }
+}
